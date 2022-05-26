73b2c704db0f052ea61cb512f26f4c08d83fc6ac
Vinicios Santos <95624273+vinicios-santos@users.noreply.github.com>
2022-05-25 22:42:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ function calculoImc() {
     console.log(altura);
     let imc = peso / (altura * altura); // fórmula de cálculo imc
 
-    if (imc < 18) { // caso o imc seja menor de 18 imprime "Abaixo do peso" 
+    if (imc < 18) {
         alert("imc:  " + imc + "  Abaixo do peso")
 
     }
-    if (imc < 25 && imc >= 18) { // caso o imc esteja entre 18 e 24 imprime "Peso Normal" 
+    if (imc < 25 && imc >= 18) {
         alert("imc:  " + imc + "  Peso Normal")
 
 
